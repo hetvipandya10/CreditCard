785b85273271dbe65c81a5e9dacb517777164641
hetvipandya10 <167962734+hetvipandya10@users.noreply.github.com>
2024-08-05 11:38:44 +0530

    Update creditcardapp.py

--- creditcardapp.py
+++ creditcardapp.py
@@ -37,7 +37,7 @@ Class=st.slider("Select Class=",45,34)
 
 if st.button("Predict"):
     import pickle
-    model=pickle.load(open("Creditcard.pkl","rb"))
+    model=pickle.load(open("creditcard.pkl","rb"))
     prd=model.predict([[Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28]])
     st.success("The Creditcard is "+ str(prd[0]))
 
